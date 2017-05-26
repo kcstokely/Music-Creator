f45fd96acfc9f7a1d8c914aa5bb8226879fab52b
Kevin Stokely <kcstokely@gmail.com>
2017-05-26 11:46:35 -0400

    Add files via upload

---
+++ music_.py
@@ -0,0 +1,365 @@
+############################################################################################################################################
+
+import numpy as np
+
+import music_write_ as mid
+
+def norm(vec):
+    return [ float(v)/float(sum(vec)) for v in vec ]
+
+############################################################################################################################################
+
+ppqn = 192
+
+# song parameters
+
+song_measures = 4*4*4
+
+beats_per_measure  = 4
+
+chord_measures_min = 1
+chord_measures_max = 6
+
+# bulk parameters
+
+b_num       = 10    # number of bulk notes (does not like zero, fix this later)
+  
+b_nroot_min = 60-24 # min pitch
+b_nroot_max = 60+12 # max pitch
+
+b_vroot_min = 32    # min velocity
+b_vroot_max = 127   # max velocity
+
+b_gnum      = 2     # number of attempted on/offs per beat
+b_gprb      = 0.01  # probability of on/off
+
+b_fnum      = 3     # number of attempted pitch changes per beat
+b_fprb      = 0.333 # probability of pitch change
+
+b_beta      = 1.25  # inverse temperature
+
+# sequence parameters
+ 
+s_num       = 8     # number of sequences
+
+s_gnum      = 1     # number of attempted on/offs per step
+s_gprb      = 0.02  # probability of on/off
+
+s_imin      = 2     # min number of notes in sequence
+s_imax      = 6     # max number of notes in sequence
+
+s_nrange    = 12    # max range of pitch
+s_nroot_min = 60-24 # min lowest pitch
+s_nroot_max = 60+12 # max lowest pitch
+
+s_vrange    = 0     # max range of velocity
+s_vroot_min = 64    # min lowest velocity
+s_vroot_max = 127   # max lowest velocity
+
+s_beta      = 1.25   # inverse temperature
+
+############################################################################################################################################
+
+###
+###  the 'energy' of two notes is equal to the LCD of the two frequencies
+###
+###    e.g., G is 3/2 the frequency of C, hence these notes have energy 2
+###          E is 5/4 the frequency of C, hence these notes have energy 4
+###
+
+elist = [1., 15., 8., 5., 4., 3., 45., 2., 5., 3., 5., 8.]
+
+def energy(m, n):
+    
+    return elist[abs(n - m) % 12]
+
+###
+###  the boltzmann factor of the sum of pairwise interactions is used as
+###    the probability of picking a particular note
+###
+
+def factor(m, n, beta):
+
+    e = 0.
+    for i in n:
+        e += energy(m, i)
+
+    return np.exp(-beta * e)
+
+###
+###  given a key, this chooses a chord
+###
+###    the root is chosen uniformly from the notes in the key
+###    the 3rd and 5th are then chosen by energy
+###
+
+ionian = [0, 2, 4, 5, 7, 9, 11]
+
+def choose_chord(key, beta):
+    
+    a = np.random.choice(map(lambda z: z+key, ionian))
+    b = np.random.choice([a+3, a+4], p = norm([factor(a+3, [key], beta), factor(a+4, [key], beta)]))
+    c = np.random.choice([a+6, a+7, a+8], p = norm([factor(a+6, [key], beta), factor(a+7, [key], beta), factor(a+8, [key], beta)]))
+
+    return map(lambda z: z%12, (a, b, c))
+
+###
+###  given a chord, this chooses a pitch to play
+###
+
+def choose_pitch(nmin, nmax, chord, beta):
+    
+    notes = range(nmin, nmax)    
+    probs = norm(map(lambda z: factor(z, chord, beta), notes))
+    
+    return np.random.choice(notes, p = probs)
+
+############################################################################################################################################
+
+if __name__ == '__main__':
+    
+    # define the key
+
+    key = 0
+    
+    # determine chord structure
+    
+    chord = []
+    
+    while len(chord) < song_measures:
+    
+        ctuple = choose_chord(key, b_beta)
+        length = np.random.choice(range(chord_measures_min, chord_measures_max))
+        
+        for _ in range(length):  
+            chord.append(ctuple)
+    
+    song_beats = len(chord) * beats_per_measure
+    
+    print 'length:', song_beats//60, 'min', song_beats%60, 'sec'
+
+    # OPEN FILE
+    
+    fp  = open('k_.mid', 'wb')
+    
+    pos = mid.w_file_head(fp, ppqn)
+    
+    # TRACK ONE: bulk
+
+    total    = mid.w_track_head(fp, beats_per_measure)
+    
+    t_now    = 0
+    t_prev   = 0
+    
+    on       = [None] * b_num
+    pitch    = [None] * b_num
+    velocity = [None] * b_num
+    
+    # set initial notes
+
+    for b in range(b_num):
+        
+        on[b]       = np.random.choice(range(2))
+        pitch[b]    = choose_pitch(b_nroot_min, b_nroot_max, chord[0], b_beta)
+        velocity[b] = np.random.choice(range(b_vroot_min, b_vroot_max))
+    
+        if on[b]:
+            
+            total += mid.w_note_on(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+            t_prev = t_now
+            t_now += ppqn//4
+    
+    t_start = t_now
+    
+    # loop through beats
+    
+    for beat in range(song_beats):
+    
+        for b in np.random.choice(range(b_num), size=b_fnum):
+            
+            if np.random.random_sample() < b_fprb:
+                
+                if on[b]:
+                    total += mid.w_note_off(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+                
+                pitch[b]    = choose_pitch(b_nroot_min, b_nroot_max, chord[beat//beats_per_measure], b_beta)
+                velocity[b] = np.random.choice(range(b_vroot_min, b_vroot_max))
+                
+                if on[b]:
+                    total += mid.w_note_on(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+    
+        for b in np.random.choice(range(b_num), size=b_gnum):
+    
+            if np.random.random_sample() < b_gprb:
+            
+                if not on[b]:
+                    
+                    on[b]  = 1
+                    total += mid.w_note_on(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+                    t_prev = t_now
+                    
+                else:
+                    
+                    on[b]  = 0
+                    total += mid.w_note_off(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+                    t_prev = t_now
+                    
+        t_now += ppqn
+                    
+    # turn shit off
+        
+    for b in range(b_num):
+            
+        if on[b]:
+            
+            print 'yep'
+            
+            on[b]  = 0
+            total += mid.w_note_off(fp, t_now-t_prev, 1, pitch[b], velocity[b])
+            t_prev = t_now
+      
+    total += mid.w_track_end(fp)
+    
+    mid.w_track_fix(fp, pos, total)
+    
+    pos += 8+total
+    
+    # TRACK TWO: sequences
+
+    total    = mid.w_track_head(fp, beats_per_measure)
+    
+    t_now    = t_start
+    t_prev   = 0
+    
+    on       = [None] * s_num
+    ind      = [None] * s_num
+    nxt      = [None] * s_num
+    skip     = [None] * s_num
+    pitch    = [None] * s_num
+    velocity = [None] * s_num
+    
+    # define sequences
+    
+    for s in range(s_num):
+
+        n = np.random.choice(range(s_imin, s_imax))
+        r = np.random.choice(range(s_nroot_min, s_nroot_max))
+        
+        p = [None] * n
+        v = [None] * n
+    
+        for i in range(n):
+            
+            p[i] = choose_pitch(r, r+s_nrange, [key], s_beta)
+            v[i] = np.random.choice(range(s_vroot_min, s_vroot_max))
+    
+        on[s]       = np.random.choice(range(2))
+        ind[s]      = 0
+        nxt[s]      = t_now
+        skip[s]     = np.random.choice([ppqn//3, ppqn//2, ppqn, ppqn*2, ppqn*3])
+        pitch[s]    = p
+        velocity[s] = v
+        
+        if on[s]:
+            
+            total += mid.w_note_on(fp, t_now-t_prev, s%16, pitch[s][ind[s]], velocity[s][ind[s]])
+            ind[s] = (ind[s]+1)%len(pitch[s])
+            nxt[s] = t_now + skip[s]
+            t_prev = t_now
+
+    # loop over divs
+    
+    for div in range(song_beats*ppqn):
+    
+        if on[s]:
+            
+            if t_now == nxt[s]:
+                
+                total += mid.w_note_off(fp, t_now-t_prev, s%16, pitch[s][ind[s]], velocity[s][ind[s]])
+                t_prev = t_now
+        
+        if div % ppqn == 0:
+        
+            for s in np.random.choice(range(s_num), size=s_gnum):
+
+                if np.random.random_sample() < s_gprb:
+
+                    if on[s]:
+
+                        on[s] = 0
+
+                    else:
+
+                        on[s]  = 1
+                        ind[s] = 0
+                        nxt[s] = t_now
+
+        if on[s]:
+            
+            if t_now == nxt[s]:
+
+                total += mid.w_note_on(fp, t_now-t_prev, s%16, pitch[s][ind[s]], velocity[s][ind[s]])
+                ind[s] = (ind[s]+1)%len(pitch[s])
+                nxt[s] = t_now + skip[s]
+                t_prev = t_now
+            
+        t_now += 1
+
+    # finish em
+    
+    for s in range(s_num):
+        
+        if on[s]:
+            
+            total += mid.w_note_off(fp, t_now-t_prev, s%12, pitch[s][ind[s]], velocity[s][ind[s]])
+            t_prev = t_now
+
+    total += mid.w_track_end(fp)
+
+    mid.w_track_fix(fp, pos, total)
+    
+    pos += 8+total
+
+    # CLOSE FILE
+    
+    fp.close()
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
+            
+            
---
+++ music_write_.py
@@ -0,0 +1,169 @@
+############################################################################################################################################
+
+import math
+
+def get_bytes(delta):
+    
+    if delta == 0:
+        
+        b = [0]
+    
+    else:
+    
+        n = int(math.floor(math.log(delta, 128) + 1))
+
+        b = [128] * (n-1)
+        b.append(0)
+        b.reverse()
+
+        for i in range(n):
+            b[i] += (delta//(128**i)) % 128
+
+        b.reverse()
+
+    return b
+
+############################################################################################################################################
+
+def w_note_on(fp, delta, channel, pitch, velocity):
+
+    # we will need this
+    blist = get_bytes(delta)
+
+    # this event happens some time after the previous event
+    fp.write(bytearray(blist))
+    
+    # this event is a note on for a particular channel with pitch / velocity
+    fp.write(bytearray([9*16+channel, pitch, velocity]))
+    
+    return len(blist)+3
+
+############################################################################################################################################
+
+def w_note_off(fp, delta, channel, pitch, velocity):
+
+    # we will need this
+    blist = get_bytes(delta)
+
+    # this event happens some time after the previous event
+    fp.write(bytearray(blist))
+    
+    # this event is a note off for a particular channel with pitch / velocity
+    fp.write(bytearray([8*16+channel, pitch, velocity]))
+    
+    return len(blist)+3
+
+############################################################################################################################################
+
+def w_track_end(fp):
+    
+    # the last event happens at the same time as the last event
+    fp.write(bytearray([0]))
+
+    # this last event is the end of the track
+    fp.write(bytearray([255, 47, 0]))
+
+    return 4
+
+############################################################################################################################################
+
+def w_track_head(fp, bpm):
+    
+    # this is a track header
+    fp.write(bytearray([77, 84, 114, 107]))
+
+    # this track is some number of bytes long
+    fp.write(bytearray([0, 0, 0, 0]))
+
+    # the first event happens at time zero
+    fp.write(bytearray([0]))
+    
+    # this first event is a tempo setting
+    fp.write(bytearray([255, 81, 3]))
+    
+    # here is the tempo info: the number of microseconds in a quarter note (hardlocked at 60 bpm right now)
+    fp.write(bytearray([15, 66, 64]))
+
+    # the second event happens at delta t zero
+    fp.write(bytearray([0]))
+
+    # this second event is a time signature
+    fp.write(bytearray([255, 88, 4]))
+    
+    # here is the time signature info
+    fp.write(bytearray([bpm])) # numerator
+    fp.write(bytearray([2]))   # denominator (log base 2)
+    fp.write(bytearray([24]))  # number of clocks in a metronome click (there are 24 clocks in a quarter note)
+    fp.write(bytearray([8]))   # number of clocks in a 32nd note (8 makes sense, but you can fuck with space-time here)
+
+    return 15;
+
+############################################################################################################################################
+
+def w_track_fix(fp, loc, total):
+
+    b = [None] * 4
+    
+    for i in range(4):
+        b[i] = (total//(256**i)) % 256
+    
+    b.reverse()
+    
+    fp.seek(loc+4)
+    fp.write(bytearray(b))
+    fp.seek(0, 2)
+
+    return 0
+        
+############################################################################################################################################
+  
+def w_file_head(fp, ppqn):
+
+    # this is a file header
+    fp.write(bytearray([77, 84, 104, 100]))
+
+    # this file header is 6 bytes long
+    fp.write(bytearray([0, 0, 0, 6]))
+
+    # this file has multiple (synchronous) tracks
+    fp.write(bytearray([0, 1]))
+        
+    # this file has two tracks in fact
+    fp.write(bytearray([0, 2]))
+
+    # this file has ppqn divs in a quarter note   
+    fp.write(bytearray([0, ppqn]))
+
+    return 14
+        
+############################################################################################################################################
+
+
+if __name__ == '__main__':
+    
+    with open('t_.mid', 'wb') as fp:
+        
+        pos = w_file_head(fp, 192)
+        
+        total = w_track_head(fp, 4)
+        
+        total += w_note_on(fp, 0, 1, 60, 127)
+
+        total += w_note_off(fp, 192*4, 1, 60, 127)
+
+        total += w_track_end(fp)
+        
+        w_track_fix(fp, pos, total)
+
+
+
+
+
+
+
+
+
+
+
+
+
